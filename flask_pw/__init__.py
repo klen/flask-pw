bf8f1a0ced7dfc26c773fcb7dd396f442b0868a1
Kirill Klenov <horneds@gmail.com>
2017-02-21 14:58:23 +0300

    Bump version: 1.0.2 → 1.0.3

--- flask_pw/__init__.py
+++ flask_pw/__init__.py
@@ -13,7 +13,7 @@ from .models import Model, BaseSignalModel, Choices # noqa
 
 __license__ = "MIT"
 __project__ = "Flask-PW"
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 LOGGER = logging.getLogger(__name__)
 
